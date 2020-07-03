635c4df61dc0145baf5075b3fca4b6b95b089680
CarlosRuiz <ingecarlosrm@gmail.com>
2020-07-03 01:13:34 -0500

    [UPDATE] Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 import '../assets/styles/components/Header.scss';
 
 const Header = () => (
-<p>header</p>
+
+  <header className='header'>
+    <figure className='logo'>
+      <img src='./assets/img/logo.png' alt='Logo DJ3' width='62px' height='38px' />
+    </figure>
+    <nav className='header__menu'>
+      <img src='./assets/img/avatar/jesusmontero.jpg' alt='' />
+      <ul>
+        <li><a href='#'>Anfitrión</a></li>
+        <li><a href='#'>Huésped</a></li>
+      </ul>
+    </nav>
+  </header>
+
 );
 
-export default Header; 
+export default Header;
