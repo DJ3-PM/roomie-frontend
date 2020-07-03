d588ebee6262eb03104daf23e7fa5ee4f38d1b7a
CarlosRuiz <ingecarlosrm@gmail.com>
2020-07-03 03:01:45 -0500

    [ADD] icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../assets/styles/components/Header.scss';
+import logo from '../assets/static/logo.png';
 
 const Header = () => (
 
   <header className='header'>
     <figure className='logo'>
-      <img src='./assets/img/logo.png' alt='Logo DJ3' width='62px' height='38px' />
+      <img src={logo} alt='Logo DJ3' width='62px' height='38px' />
     </figure>
     <nav className='header__menu'>
       <img src='./assets/img/avatar/jesusmontero.jpg' alt='' />
