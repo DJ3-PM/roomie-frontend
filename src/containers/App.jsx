d6a8d897a963e8a14408531e740d6216f294d500
Jose Cuevas <josecueram@gmail.com>
2020-07-03 19:54:53 -0500

    [ADD] Initial CreatePlace Container

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Router } from '@reach/router';
 
 import Home from './Home';
+import CreatePlace from './CreatePlace';
 import '../assets/styles/App.scss';
 
 const App = () => (
   <Router>
     <Home path='/' />
+    <CreatePlace path='/places/create' />
   </Router>
 );
 
