fcd9156d6c60d3e30919b29e25c1f36cf4b14dcb
Jose Cuevas <josecueram@gmail.com>
2020-07-04 11:21:58 -0500

    [FIX] Routes

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Router } from '@reach/router';
 import Home from '../containers/Home';
-import Login from '../containers/Signin';
+import Signin from '../containers/Signin';
 import Signup from '../containers/Signup';
 import CreatePlace from '../containers/CreatePlace';
 import CreateProfile from '../containers/CreateProfile';
 
-const App = () => (
-  <Router>
-    <Home path='/' />
-    <Signup path='/signup' />
-    <Signin path='/signin' />
-    <CreatePlace path='/places/create' />
-    <CreateProfile path='/profile/create' />
-  </Router>
-);
+const App = () => {
+  return (
+    <Router>
+      <Home path='/' />
+      <Signup path='/signup' />
+      <Signin path='/signin' />
+      <CreatePlace path='/places/create' />
+      <CreateProfile path='/profile/create' />
+    </Router>
+  );
+};
 
 export default App;
