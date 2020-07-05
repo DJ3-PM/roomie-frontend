9c77e3be2a651eae8ad565912f91ab9eab5ba3ee
Danelia Sanchez <danelia_sanchez20@hotmail.com>
2020-07-05 01:03:18 -0500

    [ADD] responsiveness

--- src/components/Carousel/styles.js
+++ src/components/Carousel/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Section = styled.section`
     display: flex;
     flex-direction: column;
+    margin: 30px auto;
 `;
 
 export const CarouselWrapper = styled.div`
