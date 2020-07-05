b7a8bb90bdadbfddd2587ca59901f303c8d24749
Jose Cuevas <josecueram@gmail.com>
2020-07-05 02:47:07 -0500

    [ADD] Basic Logic to fetch data from API in ViewRoom

--- src/containers/ViewRoom/index.jsx
+++ src/containers/ViewRoom/index.jsx
@@ -1,18 +1,62 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
 import Layout from '../../components/Layout';
 import Carousel from '../../components/Carousel';
 import Host from '../../components/Host';
 
-const ViewRoom = ({ place }) => {
-  console.log(place);
-  const { images } = place;
+const usePlaceFetch = (placeId) => {
+  const [place, setPlace] = useState({});
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPlace = async () => {
+      setLoading(true);
+      try {
+        const { data } = await axios.get(`https://peaceful-bastion-02967.herokuapp.com/api/places/${placeId}`);
+        const place = data.data;
+        const { images } = place;
+        setPlace(place);
+        setImages(images);
+        setLoading(false);
+        console.log(place);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    };
+
+    fetchPlace();
+  }, []);
+
+  return { place, images, loading };
+};
+
+const ViewRoom = ({ match }) => {
+
+  const { placeId } = match.params;
+  const { place, images, loading } = usePlaceFetch(placeId);
+
+  const renderHomeView = () => {
+    return (
+      <>
+        {
+          loading ? <h2>Loading</h2> : (
+            <>
+              <Carousel images={images} />
+              <Host {...place} />
+            </>
+          )
+        }
+      </>
+    );
+  };
 
   return (
     <Layout>
-      <Carousel images={images} />
-      <Host {...place} />
+      {renderHomeView()}
     </Layout>
   );
 };
