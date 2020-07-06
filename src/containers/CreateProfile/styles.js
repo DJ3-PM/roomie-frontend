700b69f1476b903122f0a3b4ddad0cf1daa5df22
Jose Cuevas <josecueram@gmail.com>
2020-07-05 19:56:56 -0500

    [UPDATE] Create Profile UI/UX

--- src/containers/CreateProfile/styles.js
+++ src/containers/CreateProfile/styles.js
@@ -7,3 +7,24 @@ export const Avatar = styled.img`
   object-fit: cover;
   margin: 10px auto;
 `;
+
+export const TextArea = styled.textarea`
+  resize: none;
+  font-family: inherit;
+  border: none;
+`;
+
+export const InputFile = styled.input`
+  width: 100%;
+  border-radius: 5px;
+  box-shadow: inset 0 2px 4px 0 rgba(0, 0, 0, .15);
+  font-size: 14px;
+  padding: .8em 1em;
+  box-sizing: border-box;
+  margin-bottom: 1em;
+  outline: 0;
+`;
+
+export const Label = styled.label`
+  margin-top: 15px
+`;
