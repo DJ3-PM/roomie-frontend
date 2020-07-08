e1d6213a0b4623d07dc1b937edc1f68e79600ffe
Danelia Sanchez <danelia_sanchez20@hotmail.com>
2020-07-07 20:12:07 -0500

    [FIX] Link BackToHome

--- src/components/NotFound/styles.js
+++ src/components/NotFound/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const MainContent = styled.div`
     display: flex;
@@ -26,9 +27,11 @@ export const MsgError = styled.h2`
     text-align: center;
 `;
 
-export const BtnBackHome = styled.a`
+export const BtnBackHome = styled(Link)`
     margin: 25px 0;
     background: #c3d233;
+    color: black;
+    text-decoration: none;
     padding: 15px;
     &:hover {
         font-weight: bold;
