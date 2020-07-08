4ae4124b3a0fe4d6ea3eba800ebfa894ba56862d
Jose Cuevas <josecueram@gmail.com>
2020-07-08 18:21:09 -0500

    [ADD] Carousel Buttons functionality

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { AiFillCaretLeft as Left, AiFillCaretRight as Right } from 'react-icons/ai';
 import CarouselItem from '../CarouselItem';
 import ImagesList from '../ImagesList';
-import { Section, CarouselWrapper, BtnLeftWrapper, BtnRightWrapper, Button, BtnImg, Image } from './styles';
+import { Section, CarouselWrapper, BtnLeftWrapper, BtnRightWrapper, Button, Image } from './styles';
 
 const Carousel = ({ images = [] }) => {
   const [mainImage, setMainImage] = useState(images[0]);
@@ -12,16 +12,32 @@ const Carousel = ({ images = [] }) => {
     setMainImage(images[target.id]);
   };
 
+  // This could be improved...
+  const handleLeft = () => {
+    let newPosition = images.indexOf(mainImage) - 1;
+    if (newPosition < 0) {
+      newPosition = images.length - 1;
+    }
+    setMainImage(images[newPosition]);
+  };
+  const handleRight = () => {
+    let newPosition = images.indexOf(mainImage) + 1;
+    if (newPosition >= images.length) {
+      newPosition = 0;
+    }
+    setMainImage(images[newPosition]);
+  };
+
   return (
     <Section>
       <CarouselWrapper>
-        <BtnLeftWrapper>
+        <BtnLeftWrapper onClick={handleLeft}>
           <Button>
             <Left size='25px' color='#BACD25' />
           </Button>
         </BtnLeftWrapper>
         <CarouselItem active={true} id='image1' image={mainImage} />
-        <BtnRightWrapper>
+        <BtnRightWrapper onClick={handleRight}>
           <Button>
             <Right size='25px' color='#BACD25' />
           </Button>
