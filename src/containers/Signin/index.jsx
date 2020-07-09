c2d1211458e64c20fb5d6010fb41754af592b217
Jose Cuevas <josecueram@gmail.com>
2020-07-09 17:02:54 -0500

    [ADD] POST request  to api/signin

--- src/containers/Signin/index.jsx
+++ src/containers/Signin/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 import Layout from '../../components/Layout';
 import Wrapper from '../../components/Wrapper';
@@ -7,12 +8,47 @@ import Input from '../../components/Input';
 import FormButton from '../../components/FormButton';
 
 const Signin = () => {
+  const [form, setForm] = useState({});
+
+  const handleOnSubmit = async (event) => {
+    event.preventDefault();
+    const { username, password } = form;
+    console.log('password', password);
+    console.log('username', username);
+
+    try {
+      const { data } = await axios({
+        url: 'http://localhost:8000/api/auth/sign-in',
+        method: 'post',
+        auth: {
+          username,
+          password,
+        },
+      });
+      console.log('data', data);
+
+    } catch (error) {
+      console.log(error);
+    }
+
+  };
+
+  const handleTextInput = (event) => {
+    const { target } = event;
+
+    setForm({
+      ...form,
+      [target.name]: target.value,
+    });
+  };
+
+  console.log(form);
   return (
     <Layout>
       <Wrapper>
-        <Form action='' title='Sign in'>
-          <Input type='email' placeholder='Email' required />
-          <Input type='password' placeholder='Password' required />
+        <Form onSubmit={handleOnSubmit} title='Sign in'>
+          <Input name='username' onChange={handleTextInput} type='email' placeholder='Email' required />
+          <Input name='password' onChange={handleTextInput} type='password' placeholder='Password' required />
           <FormButton text='Sign in!' />
         </Form>
       </Wrapper>
