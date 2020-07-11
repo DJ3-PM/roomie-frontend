61d1525626d6bca0cfa4f4c02bd5acf4f4ef06d6
Jose Cuevas <josecueram@gmail.com>
2020-07-11 17:09:18 -0500

    [ADD] React-Helmet

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Header from '../Header';
 import Footer from '../Footer';
 
-const Layout = ({ children }) => {
+const Layout = ({ title = '', description, children }) => {
   return (
     <>
+      <Helmet>
+        <title>
+          {`🏘 Roomeate | ${title}`}
+        </title>
+        <meta name='description' content={description} />
+        <link href='https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap' rel='stylesheet' />
+      </Helmet>
       <Header />
 
       { children }
