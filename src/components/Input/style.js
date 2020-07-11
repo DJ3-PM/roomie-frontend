7a5969915ab52537d54a0b851823ed347c0b608d
CarlosRuiz <ingecarlosrm@gmail.com>
2020-07-11 04:16:35 -0500

    [UPDATE] Form styles

--- src/components/Input/style.js
+++ src/components/Input/style.js
@@ -5,10 +5,12 @@ export const Input = styled.input`
   margin: 10px 0;
   border: 0;
   padding: 20px 15px;
+  background-color: #fff;
+  border-radius: 10px;
 
   &:focus {
-    outline: 1px solid #BACD25;
-
+    outline: 2px solid #3925cd;
+    
   }
 `;
 
