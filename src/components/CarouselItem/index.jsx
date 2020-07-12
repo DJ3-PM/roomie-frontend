6797bd622b53648a63ef14ec4eb3c7c5434f668e
Jose Cuevas <josecueram@gmail.com>
2020-07-11 21:13:14 -0500

    [ADD] PropTypes -> CarouselItem

--- src/components/CarouselItem/index.jsx
+++ src/components/CarouselItem/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Image, ImgWrapper } from './styles';
 
 const CarouselItem = ({ image }) => {
   return (
-    <ImgWrapper id='image1'>
-      <Image src={image} alt='image1 del room' />
+    <ImgWrapper>
+      <Image src={image} alt='Place image' />
     </ImgWrapper>
   );
+};
+
+CarouselItem.propTypes = {
+  image: PropTypes.string.isRequired,
 };
 
 export default CarouselItem;
