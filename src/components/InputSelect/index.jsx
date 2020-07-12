34ebd6606452da0a4c3ef3fdb9da95fa7e874c6b
Jose Cuevas <josecueram@gmail.com>
2020-07-11 21:04:29 -0500

    [ADD] PropTypes -> InputSelect

--- src/components/InputSelect/index.jsx
+++ src/components/InputSelect/index.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Select } from './styles';
 
@@ -11,6 +12,12 @@ const InputSelect = ({ name, optionsArray, onChange }) => {
       }
     </Select>
   );
+};
+
+InputSelect.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  optionsArray: PropTypes.arrayOf(PropTypes.string).isRequired,
+  name: PropTypes.string,
 };
 
 export default InputSelect;
